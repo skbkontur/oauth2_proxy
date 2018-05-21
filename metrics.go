4118303c643a541d3a2be636b03f36b1994f8044
kamaev <hakariaruki@protonmail.com>
2018-05-21 14:20:28 +0500

    fix basic auth metrics statements

--- metrics.go
+++ metrics.go
@@ -10,16 +10,18 @@ import (
 )
 
 func (p *OAuthProxy) incrementAuthenticated(session *providers.SessionState, method string) {
-	if p.PassBasicAuth {
-		userAlias := unidecode.Unidecode(session.User)
-		userAlias = strings.Replace(userAlias, ".", "-", -1)
-		metricName := fmt.Sprintf("authenticated.BasicAuth.%s.%s", userAlias, method)
-		p.StatsD.Increment(metricName)
-	} else {
+	if len(session.Groups) > 0 {
 		for _, group := range session.Groups {
 			groupAlias := unidecode.Unidecode(group)
 			groupAlias = strings.Replace(groupAlias, ".", "-", -1)
 			metricName := fmt.Sprintf("authenticated.Oauth2.%s.%s", groupAlias, method)
+			p.StatsD.Increment(metricName)
+		}
+	} else {
+		if p.PassBasicAuth && len(session.User) > 0 {
+			userAlias := unidecode.Unidecode(session.User)
+			userAlias = strings.Replace(userAlias, ".", "-", -1)
+			metricName := fmt.Sprintf("authenticated.BasicAuth.%s.%s", userAlias, method)
 			p.StatsD.Increment(metricName)
 		}
 	}
